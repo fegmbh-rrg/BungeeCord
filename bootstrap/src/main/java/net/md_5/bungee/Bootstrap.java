1888e3a6364d99164d09a5225de5643d3431f97b
FeGmbH <mr.quatsch.officional@gmail.com>
2026-06-23 20:37:34 +0200

    Implement restart loop in Bootstrap class

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -2,7 +2,6 @@ package net.md_5.bungee;
 
 public class Bootstrap
 {
-
     public static void main(String[] args) throws Exception
     {
         if ( Float.parseFloat( System.getProperty( "java.class.version" ) ) < 61.0 )
@@ -12,6 +11,52 @@ public class Bootstrap
             return;
         }
 
-        BungeeCordLauncher.main( args );
+        while (true)
+        {
+            try
+            {
+                BungeeCord.isRestarting = false;
+                
+                BungeeCordLauncher.main( args );
+                
+                waitForShutdown();
+            }
+            catch (RestartSignalException e)
+            {
+                System.out.println( "[Bootstrap] Restart-Signal empfangen. Bereite Re-Bootstrapping vor..." );
+                prepareForReboot();
+            }
+            catch (Exception e)
+            {
+                System.err.println( "[Bootstrap] Unerwarteter Fehler im Lebenszyklus:" );
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
+    }
+
+    private static void waitForShutdown()
+    {
+        try
+        {
+            while (BungeeCord.getInstance() != null)
+            {
+                Thread.sleep(250);
+            }
+        }
+        catch (InterruptedException e)
+        {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private static void prepareForReboot()
+    {
+        System.gc();
+        try
+        {
+            Thread.sleep(1000);
+        }
+        catch (InterruptedException ignored) {}
     }
 }
